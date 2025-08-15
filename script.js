ffa5d4cb6e719a9cfabb10fd9b6c55f307a6c5e4
Santailha <vendas@santailha.com.br>
2025-08-15 20:12:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// --- CONFIGURAÇÃO DO FIREBASE (COLE A SUA AQUI) ---
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_AUTH_DOMAIN",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_STORAGE_BUCKET",
-    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-    appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAyXJ1amVIFr588r_MkN_qVvyxbyOQU1KM",
+  authDomain: "calendario-corretores.firebaseapp.com",
+  projectId: "calendario-corretores",
+  storageBucket: "calendario-corretores.firebasestorage.app",
+  messagingSenderId: "356393596100",
+  appId: "1:356393596100:web:3e82de3465b6f057c44591"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
